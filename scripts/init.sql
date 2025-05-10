cf3f1587b85274a6a71e43ba8b298e81b85eec23
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-05-09 21:16:18 -0300

    fix: SQL

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,27 +1,33 @@
 -- criação da tabela de  usuário
 CREATE TABLE IF NOT EXISTS usuário (
-  user_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   nome VARCHAR(50) NOT NULL,
   email VARCHAR(200),
   senha VARCHAR(200)
 );
 
+-- ciaração da tabela de categorias
+CREATE TABLE IF NOT EXISTS categorias (
+  id SERIAL PRIMARY KEY,
+  título_categoria VARCHAR(100) NOT NULL,
+  descrissão TEXT
+);
+
 -- criação da tabela de atividades
 CREATE TABLE IF NOT EXISTS atividades (
-  task_id SERIAL PRIMARY KEY,
-  usuario VARCHAR(50),
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(50),
   categoria VARCHAR(50),
-  título VARCHAR(50),
+  título VARCHAR(50) NOT NULL,
   descrissão TEXT,
   prazo TIMESTAMP,
   prioridade INTEGER,
   concluido BOOLEAN,
-  criado_em TIMESTAMP
+  criado_em TIMESTAMP,
+  id_usuário INT,
+  id_categoria INT,
+  FOREIGN KEY (id_usuário) REFERENCES usuário(id),
+  FOREIGN KEY (id_categoria) REFERENCES categorias(id)
 );
 
--- ciaração da tabela de categorias
-CREATE TABLE IF NOT EXISTS categorias (
-  category_id SERIAL PRIMARY KEY,
-  título_categoria VARCHAR(100),
-  descrissão TEXT
-);
+
