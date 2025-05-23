476f97bba6d0853ac65993ea933c63ac0091ad8a
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-05-23 11:38:38 -0300

    update legendas userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const userController = require('../controllers/userController');
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
+router.put('/:id', userController.updateUser); // o ":id" indica que é um parâmetro de rota e deve ser trocado por um valor
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
