a293d779acd0963077025c0a54b74aea74864f54
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-05-23 18:04:19 -0300

    Update init.sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -9,26 +9,15 @@ CREATE TABLE IF NOT EXISTS usuario (
 -- ciaração da tabela de categorias
 CREATE TABLE IF NOT EXISTS categorias (
   id SERIAL PRIMARY KEY,
-  título_categoria VARCHAR(100) NOT NULL,
-  descrissão TEXT
+  titulo_categoria VARCHAR(100) NOT NULL,
+  descrissao TEXT
 );
 
 -- ciaração da tabela de categorias
 CREATE TABLE IF NOT EXISTS projetos (
   id SERIAL PRIMARY KEY,
-  título_projeto VARCHAR(100) NOT NULL,
-  descrissão TEXT
-);
-
--- criação da tabela de atividades
-CREATE TABLE IF NOT EXISTS subAtividades (
-  id SERIAL PRIMARY KEY,
-  título VARCHAR(50) NOT NULL,
-  descrissão TEXT,
-  prazo TIMESTAMP,
-  prioridade INTEGER,
-  concluido BOOLEAN,
-  criado_em TIMESTAMP
+  titulo_projeto VARCHAR(100) NOT NULL,
+  descrissao TEXT
 );
 
 -- criação da tabela de atividades
@@ -43,9 +32,20 @@ CREATE TABLE IF NOT EXISTS atividades (
   id_usuario INT,
   id_categoria INT,
   id_projeto INT,
-  id_subAtividade INT,
   FOREIGN KEY (id_usuario) REFERENCES usuario(id),
   FOREIGN KEY (id_categoria) REFERENCES categorias(id),
-  FOREIGN KEY (id_projeto) REFERENCES projetos(id),
-  FOREIGN KEY (id_subAtividade) REFERENCES subAtividades(id)
+  FOREIGN KEY (id_projeto) REFERENCES projetos(id)
+);
+
+-- criação da tabela de atividades
+CREATE TABLE IF NOT EXISTS subAtividades (
+  id SERIAL PRIMARY KEY,
+  titulo VARCHAR(50) NOT NULL,
+  descrissao TEXT,
+  prazo TIMESTAMP,
+  prioridade INTEGER,
+  concluido BOOLEAN,
+  criado_em TIMESTAMP,
+  id_subAtividades INT,
+  FOREIGN KEY (id_subAtividades) REFERENCES atividades(id)
 );
